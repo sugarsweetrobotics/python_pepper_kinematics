a9b6c5a3876b7e2225f4ad371d805a8b99a9e662
Yuki Suga <ysuga@ysuga.net>
2015-10-27 16:38:18 +0900

    setup.py added

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import sys
+
+
+setup(name='pepper_kinematics',
+      version='0.0.1',
+      url = 'http://www.sugarsweetrobotics.com/',
+      author = 'ysuga',
+      author_email = 'ysuga@ysuga.net',
+      description = 'Pepper Inverse/Forward Kinematics library',
+      download_url = 'https://github.com/sugarsweetrobotics/python_pepper_kinematics',
+      packages = find_packages("pepper_kinematics"),
+      py_modules = ["pepper_kinematics"],
+      license = 'GPLv3',
+      install_requires = [numpy],
+      classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+        ],
+      #test_suite = "foo_test.suite",
+      #package_dir = {'': 'src'}
+    )
